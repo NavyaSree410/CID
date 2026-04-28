f1d3b2d51e67d2a937b760f45c734636c7264fa0
Navya Sree <navyasree9490@gmail.com>
2026-04-29 04:04:39 +0530

    Rename function from generate_mlat_pdf to generate_mlat

--- mlat_generator.py
+++ mlat_generator.py
@@ -1,17 +1,16 @@
 from fpdf import FPDF
 
-def generate_mlat_pdf(case):
+
+def generate_mlat(case):
     pdf = FPDF()
     pdf.add_page()
-
     pdf.set_font("Arial", size=12)
 
-    pdf.cell(200, 10, txt="MLAT REQUEST REPORT", ln=True, align='C')
-
+    pdf.cell(200, 10, "MLAT REQUEST REPORT", ln=True, align="C")
     pdf.ln(10)
 
-    for key, value in case.items():
-        pdf.cell(200, 10, txt=f"{key}: {value}", ln=True)
+    for k, v in case.items():
+        pdf.cell(200, 10, f"{k}: {v}", ln=True)
 
     filename = f"MLAT_{case['complaint_id']}.pdf"
     pdf.output(filename)
