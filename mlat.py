4b41e64f1ab647717415b05b499238607dd8f8a5
Navya Sree <navyasree9490@gmail.com>
2026-04-29 04:44:49 +0530

    Rename function and update PDF title and filename

--- mlat.py
+++ mlat.py
@@ -1,18 +1,18 @@
 from fpdf import FPDF
 
 
-def generate_mlat(case):
+def generate(case):
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size=12)
 
-    pdf.cell(200, 10, "MLAT REPORT", ln=True, align="C")
+    pdf.cell(200, 10, "MLAT REQUEST DOCUMENT", ln=True, align="C")
     pdf.ln(10)
 
     for k, v in case.items():
         pdf.cell(200, 10, f"{k}: {v}", ln=True)
 
-    file = f"MLAT_{case['complaint_id']}.pdf"
+    file = f"MLAT_{case['case_id']}.pdf"
     pdf.output(file)
 
     return file
