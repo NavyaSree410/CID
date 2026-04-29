6deac65bcc3519a43458f5e9274e9338b40bda7b
Navya Sree <navyasree9490@gmail.com>
2026-04-29 07:29:45 +0530

    Refactor trigger_alert function parameters and structure

--- alert.py
+++ alert.py
@@ -1,13 +1,10 @@
 alerts = []
 
 
-def trigger_alert(case_id, location, police_unit):
-    alert = {
+def trigger_alert(case_id, location, unit):
+    alerts.append({
         "case_id": case_id,
         "location": location,
-        "police_unit": police_unit,
-        "status": "ACTIVE MLAT ALERT"
-    }
-
-    alerts.append(alert)
-    return alert
+        "unit": unit,
+        "status": "ACTIVE"
+    })
