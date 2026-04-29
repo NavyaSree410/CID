64b027e92f494856b6e0f95467fe85464ae9a388
Navya Sree <navyasree9490@gmail.com>
2026-04-29 07:45:50 +0530

    Simplify block addition by appending data directly
    Removed block creation logic and directly appended data to the chain.

--- blockchain.py
+++ blockchain.py
@@ -1,14 +1,4 @@
-import hashlib
-import json
-
 chain = []
 
-
 def add_block(data):
-    prev = chain[-1]["hash"] if chain else "0"
-
-    raw = json.dumps(data) + prev
-    h = hashlib.sha256(raw.encode()).hexdigest()
-
-    block = {"data": data, "prev": prev, "hash": h}
-    chain.append(block)
+    chain.append(data)
