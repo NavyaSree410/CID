c4718857b111efdd69728d57bcf1b4c7394dac6a
Navya Sree <navyasree9490@gmail.com>
2026-04-29 07:32:43 +0530

    Refactor block structure to exclude timestamp
    Removed timestamp from block data structure.

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,5 @@
 import hashlib
 import json
-from datetime import datetime
 
 chain = []
 
@@ -8,15 +7,8 @@ chain = []
 def add_block(data):
     prev = chain[-1]["hash"] if chain else "0"
 
-    raw = json.dumps(data) + prev + str(datetime.now())
+    raw = json.dumps(data) + prev
     h = hashlib.sha256(raw.encode()).hexdigest()
 
-    block = {
-        "data": data,
-        "prev_hash": prev,
-        "hash": h,
-        "time": str(datetime.now())
-    }
-
+    block = {"data": data, "prev": prev, "hash": h}
     chain.append(block)
-    return block
